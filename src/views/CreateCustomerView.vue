43217f68d99b5b93dd7b3acdac9fa198be9e3b32
Abderrazzaq Khastaf <abderrazzaqkhastaf@Abderrazzaqs-MacBook-Pro.local>
2022-11-01 20:35:18 +0100

    fix the customer subscribe

--- src/views/CreateCustomerView.vue
+++ src/views/CreateCustomerView.vue
@@ -1,31 +1,46 @@
 <script setup lang="ts">
 import axiosClient from '@/helpers/axios';
 import router from '@/router';
-import { onMounted, reactive, ref } from 'vue';
+import { onBeforeMount, onMounted, reactive, ref } from 'vue';
 import Modal from '../components/Modal.vue';
 import { DialogTitle } from '@headlessui/vue';
 import { useRoute } from 'vue-router';
+import type { User } from '@/types';
 
 const route = useRoute();
 const init = {
   full_name: '',
-  phone: ''
+  phone: '',
 };
 const initialFormErrors = {
-    full_name: '',
-    phone: '',
+  full_name: '',
+  phone: '',
 };
 const form = reactive(init)
 
+const code = ref('+1');
 const open = ref(false);
+const store = ref<User>();
 const error = ref(false);
 const errorMessage = ref('');
 const formError = reactive(initialFormErrors);
+const storeNotFound = ref(false);
 
+onBeforeMount(() => {
+  axiosClient.get(`/users/${route.params.id}`).then((data) => {
+    store.value = data.data;
+  }).catch((err) => {
+    storeNotFound.value = true;
+    error.value = true;
+    errorMessage.value = err.response.data.message;
+    console.log(err);
+  });
+});
 
 function create() {
     errorMessage.value = '';
     Object.assign(formError, initialFormErrors);
+    form.phone = code.value + form.phone;
     axiosClient.post(`/customers/${route.params.id}`, form)
         .then((data) => {
             Object.assign(form, init);
@@ -36,7 +51,6 @@ function create() {
             parseError(err.response.data.message);
           else
           {
-            open.value = true;
             error.value = true;
             errorMessage.value = err.response.data.message;
           }
@@ -55,8 +69,6 @@ const parseError = (errors: string[]) => {
 
 const close = () => {
   open.value = false;
-  errorMessage.value = '';
-  error.value = false;
 }
 
 </script>
@@ -64,13 +76,17 @@ const close = () => {
 <template>
   <main class="container mx-auto w1/2">
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div class="w-full max-w-md space-y-8">
+      <div class="w-full max-w-md">
         <div>
           <img class="mx-auto h-24 w-auto" src="../assets/logo.png"  alt="Your Company">
-          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">{{ $t('subscribe_title') }}</h2>
+          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">{{ `${$t('subscribe_title')} ${ !storeNotFound ? store?.name + ' store' : ''}` }}</h2>
+          <img v-if="!storeNotFound" :src="store?.avatar" class="mx-auto mt-4 text-center h-16 w-16 rounded-full" alt="store avatar"/>
+        </div>
+        <div v-if="error" class="text-red-600 mx-auto text-center">
+          <p>{{ errorMessage }}</p>
         </div>
         <div class="bg-white p-4">
-          <form @submit.prevent="create" class="mt-4 space-y-6" action="#" method="POST">
+          <form @submit.prevent="create" class="md:mt-4 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true">
             <div class="-space-y-px rounded-md shadow-sm">
               <div class="mb-6">
@@ -82,7 +98,13 @@ const close = () => {
               </div>
               <div>
                 <label for="phone" class="font-semibold">{{ $t('phone') }}</label>
-                <input :class="formError.phone.length ? 'border-red-300' : 'border-gray-300'" id="phone" v-model="form.phone" name="phone" type="phone" autocomplete="current-phone" required class="mt-4 relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                <div class="flex">
+                  <select  v-model="code" class="w-auto border-gray-300 mt-4 mr-4 relative block appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                    <option value="+1">United state (+1)</option>
+                    <option value="+212">Morocco (+212)</option>
+                  </select>
+                  <input :class="formError.phone.length ? 'border-red-300' : 'border-gray-300'" id="phone" v-model="form.phone" name="phone" type="phone" required class="mt-4 relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                </div>
                 <p v-if="formError.phone.length" class="peer-invalid:visible text-red-700 font-light">
                   {{ formError.phone }}
                 </p>
@@ -97,9 +119,6 @@ const close = () => {
               <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                   <!-- Heroicon name: mini/lock-closed -->
-                  <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-                  </svg>
                 </span>
                 {{ $t('subscribe')}}
               </button>
@@ -112,21 +131,15 @@ const close = () => {
   <Modal :open="open" @close-modal="close">
     <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
         <div class="sm:flex sm:items-start">
-        <div :class="error ? 'bg-red-100' : 'bg-green-100'" class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
-            <svg v-if="error" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-            </svg>
-
-            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-green-600">
+        <div class="mx-auto flex h-12 w-12 bg-green-100 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-green-600">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
             </svg>
         </div>
         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-            <DialogTitle v-if="!error" as="h3" class="text-lg font-medium leading-6 text-gray-900">{{ $t('congratulation') }}</DialogTitle>
-            <DialogTitle v-else as="h3" class="text-lg font-medium leading-6 text-gray-900">{{ $t('error') }}</DialogTitle>
-            <div class="mt-2">
-            <p v-if="error" class="text-sm text-gray-500">{{ errorMessage }}</p>
-            <p v-else class="text-sm text-gray-500">{{ $t('success_subscribe') }}</p>
+            <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">{{ $t('congratulation') }}</DialogTitle>
+             <div class="mt-2">
+            <p class="text-sm text-gray-500">{{ $t('success_subscribe') }}</p>
             </div>
         </div>
         </div>
